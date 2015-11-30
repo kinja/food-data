8ae65eae2263f9b5f93c51ed4c8398b0fb31903c
Damian Rzeszot <damian.rzeszot@gmail.com>
2015-11-30 18:52:17 +0100

    Add generator for manufacturers

--- generate.rb
+++ generate.rb
@@ -3,45 +3,90 @@ require 'erb'
 require 'awesome_print'
 require 'json'
 
+def symbolize(obj)
+  case obj
+  when Hash
+    obj.map { |k, v| [k.respond_to?(:to_sym) ? k.to_sym : k, symbolize(v) ]}.to_h
+  when Enumerable
+    obj.map { |x| symbolize(x) }
+  else
+    obj
+  end
+end
+
 def yaml(file)
   document = IO.read(file)
   content = ERB.new(document).result
-  data = YAML.load content
+  data = symbolize YAML.load(content)
 end
 
 def json(data)
   JSON.pretty_generate data
 end
 
+#
+#
+#
+
 def types
-  input = yaml "types.yml"
+  @types ||= yaml("types.yml").map do |key, type|
+    type[:parent] = type[:parent].to_sym if type.key? :parent
+    [key, type]
+  end.to_h
+end
 
-  input.each_with_index.map do |entry, index|
-    key, data = entry
+def manufacturers
+  @files ||= Dir.glob 'manufacturers/*.yml'
 
-    id = index+1
-    name = data['name']
+  @manufacturers ||= @files.map do |file|
+    name = "manufacturer_" + File.basename(file, '.yml')
+    data = yaml file
 
-    parent_key = data['parent']
+    [ name.to_sym, data ]
+  end.to_h
+end
 
-    parent_id = input.keys.index(parent_key)
-    parent_id += 1 unless parent_id.nil?
+def products
+  @files ||= Dir.glob 'products/*.yml'
 
-    parent = input[parent_key]
+  @products ||= @files.map do |file|
+    name = "product_" + File.basename(file, '.yml')
+    data = yaml file
+
+    [ name.to_sym, data ]
+  end.to_h
+end
+
+#
+#
+#
+
+def types_data
+  types.map.each_with_index do |entry, index|
+    _key, data = entry
 
     output = {
-      id: id,
-      name: name
+      id: index+1,
+      name: data[:name],
     }
 
-    if not parent_id.nil?
-      output[:parent_id] = parent_id
+    output[:unit] = data[:unit] if data.key? :unit
+
+    if data.key? :parent
+      parent = data[:parent]
+      output[:parent_id] = types.keys.index(parent) + 1
     end
 
     output
   end
 end
 
+def manufacturers_data
+  manufacturers.map.each_with_index do |entry, index|
+    _key, data = entry
+    { id: index + 1 }.merge(data)
+  end
+end
 
-
-File.write 'data/types.json', json(types)
+File.write 'generated/types.json', json(types_data)
+File.write 'generated/manufacturers.json', json(manufacturers_data)
